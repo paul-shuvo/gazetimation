0a3a1354fdbe9765cf5f53f0bb641f5f79b4eb64
Shuvo Kumar Paul <shuvo.k.paul@gmail.com>
2022-08-26 21:26:30 -0700

    docs: initial deployment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ import sys
 _HERE = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(_HERE, '../../src'))
 
-# import gazetimation
+import gazetimation
 
 project = "Gazetimation"
 copyright = "2022, Shuvo Kumar Paul"
