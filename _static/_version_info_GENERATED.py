b7ec41f77a54b2546d81cb003e5e8915b09487ef
paul-shuvo <paul-shuvo@users.noreply.github.com>
2022-08-29 15:51:08 +0000

    deploy: 2181239f99bd21949281ec944308cf4e611be022

---
+++ _static/_version_info_GENERATED.py
@@ -0,0 +1,10 @@
+version_info = (
+{
+  "build_mtime": "1657726028",
+  "module_name": "sphinx_typo3_theme",
+  "version_scm": "4.7.5",
+  "version_scm_build": "",
+  "version_scm_core": "4.7.5",
+  "version_scm_pre_release": ""
+}
+)
